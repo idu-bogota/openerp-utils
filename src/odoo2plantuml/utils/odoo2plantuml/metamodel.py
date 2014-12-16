3cce089473e45cb870999a931019a189ae65206d
cjvargas3 <javier.vargas@idu.gov.co>
2014-12-16 14:55:46 -0500

    see #11 corrección Exclude en modelo uml

--- src/odoo2plantuml/utils/odoo2plantuml/metamodel.py
+++ src/odoo2plantuml/utils/odoo2plantuml/metamodel.py
@@ -3,22 +3,46 @@ Created on 21/11/2014
 
 @author: jota
 '''
+import re
 
 class Model():
 
-    def __init__(self, model, file, detailed_model):
+    def __init__(self, model, file, detailed_model, model_exclude):
         self.model = model
         self.file = file
         self.detailed_model = detailed_model
+        self.model_exclude = model_exclude
+    #excluir
+    def exclude_of_graphic(self, relacion):
+        retorno = True
+        
+        entity_to_exclude = self.model_exclude.split(',')
+        
+        for exclude in entity_to_exclude:
+            if '*' in exclude:
+                if re.match(exclude, relacion):
+                    #print "esta en el exclude    {0}".format(relacion)
+                    retorno = False
+                    return retorno
+            else:
+                if relacion in entity_to_exclude:
+                    #print "esta en el exclude    {0}".format(relacion)
+                    retorno = False
+                    return retorno
+        return retorno
+    
     #grafica las entida
     def get_plantuml_relation_tags(self):
         for i in self.model:
-            # print i
-            # print self.model[i]
             campos = self.model[i]
             for key, campo in campos.iteritems():
                 if campo['type'] == 'many2one':
-                    self.file.write("{0} \"*\" -- {1}\n".format(i, campo['relation']))
+                    #excluir 
+                    if self.model_exclude:
+                        if self.exclude_of_graphic(campo['relation']):
+                            self.file.write("{0} \"*\" -- {1}\n".format(i, campo['relation']))
+                    else:
+                        self.file.write("{0} \"*\" -- {1}\n".format(i, campo['relation']))
             self.file.write("\n")
     
     def get_plantuml_entity_tags(self):
--- src/odoo2plantuml/utils/odoo2plantuml/uml.py
+++ src/odoo2plantuml/utils/odoo2plantuml/uml.py
@@ -15,7 +15,7 @@ _logger = logging.getLogger('script')
 
 def main():
     
-    usage = "Connection to Active Directory by LDAP\nusage: %prog [options]"
+    usage = "PlantUml creating model objects starting business Odoo: %prog [options]"
     parser = OptionParser(usage)
     
     parser.add_option("-N", "--db_name", dest="db_name", help="OpenERP database name")
@@ -44,13 +44,7 @@ def main():
         parser.error('db_password not given')
     if not options.model_openERP:
         parser.error('model_openERP not given')
-    '''
-    if int(options.model_levels) > 3:    #int(float(a))
-        parser.error('model_levels must be less than 3')
-    if int(options.model_levels) <= 0:    #int(float(a))
-        parser.error('model_levels must be greater than 0')    
-    '''    
-        
+
     p = get_details_db(options) 
 
     ## fin main
@@ -145,7 +139,7 @@ def clean_entity_of_model_general(entity_relations, model):
     return end_entity_relations 
 
 def assign_new_entity(connect, entity_relations, model):
-    _logger.debug( "\n*** assign_new_entity ***")
+    _logger.debug( "\n*** assign_new_entity ***\n")
     
     for i in entity_relations:
         _logger.debug("{0} Campos agregado".format(i))
@@ -154,7 +148,7 @@ def assign_new_entity(connect, entity_relations, model):
     return model
 
 def exclude_entity_of_model_general(options, model):
-    _logger.debug("exclude_entity_of_model_general")
+    _logger.debug("\n*** exclude_entity_of_model_general ***")
     
     entity_to_exclude = options.model_exclude.split(',')
     
@@ -171,25 +165,20 @@ def exclude_entity_of_model_general(options, model):
     return end_model
     
 def include_entity_of_model_general(options, model):
-    _logger.debug("include_entity_of_model_general")
+    _logger.debug("\n*** include_entity_of_model_general ***")
     c = get_connection(options)
     # separar por comas
     entity_to_include = options.model_include.split(',')
     
     for include in entity_to_include:
         if '*' in include:
-            print '\n***** consultar en ir model ******\n'
             end_entity_to_include = get_model_of_db_exr(options, include, c)
-            
             end_entity_to_include = clean_entity_of_model_general(end_entity_to_include, model)
-            
             end_model = assign_new_entity(c, end_entity_to_include, model)
         else:
             lista = []
             lista.insert(0,include) # se convierte en lista porque las funciones esperan listas
-
             end_entity_to_include = clean_entity_of_model_general(lista, model)
-            
             end_model = assign_new_entity(c, end_entity_to_include, model)
     
     return end_model
@@ -198,14 +187,12 @@ def get_model_of_db_exr(options, models_to_consult, connect):
     end_record_model = []
     models = connect.model('ir.model')
     models_to_consult = models_to_consult.replace('*', '%')  #cambiar el * por el %
-    
     record_model = models.browse([ "model like '{0}'".format(models_to_consult) ])
     
     for i in record_model:
         end_record_model.append(i.model)
     
     return end_record_model
-
 
 def get_details_db(options):
     
@@ -215,19 +202,18 @@ def get_details_db(options):
     for i in range(profundidad):
         print "{0}  {1}\n".format("Iteracion numero :", i)
         a = generate_model(options.model_openERP, a, options)
-    # ***
+
     _logger.debug("\n*** retorno generate_model ***")
     for i in a:
-        print i
-        
-        
+        _logger.debug(i)
+
     # include
     if(options.model_include):
         a = include_entity_of_model_general(options, a)
 
         _logger.debug("\n*** retorno despues del include ***")
         for i in a:
-            print i
+            _logger.debug(i)
 
     # exclude
     if(options.model_exclude):
@@ -235,10 +221,10 @@ def get_details_db(options):
     
         _logger.debug("\n*** retorno despues del exclude ***")
         for i in a:
-            print i
+            _logger.debug(i)
     
     file = init_graph()
-    pintar = Model(a, file, options.detailed_model)
+    pintar = Model(a, file, options.detailed_model, options.model_exclude )
     pintar.get_plantuml_relation_tags()
     pintar.get_plantuml_entity_tags()
     
