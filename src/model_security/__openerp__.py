4ea94f13565c46b09d4f1914bb0e55ee2f382be9
Cinxgler Mariaca Minda <cinxgler@gmail.com>
2015-09-10 17:14:12 -0500

    Added model_security module

---
+++ src/model_security/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    "name" : "Extra Model Security",
+    "version" : "odoo8.0-rev2015060900",
+    "author" : "Instituto de Desarrollo Urbano - STRT I+D+I",
+    "category" : "idu", 
+    "description" : """Module to add extra security features to models
+    """,
+    "depends" : [
+        'base',
+    ],
+    "data" : [
+    ],
+    'test': [
+    ],
+    'demo': [
+    ],
+    "installable" : True,
+    "auto_install": True,
+}
---
+++ src/model_security/model_security.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Grupo de Investigación, Desarrollo e Innovación I+D+I
+#    Subdirección de Recursos Tecnológicos - STRT
+#    INSTITUTO DE DESARROLLO URBANO - BOGOTA (COLOMBIA)
+#    Copyright (C) 2015 IDU STRT I+D+I (http://www.idu.gov.co/)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.exceptions import AccessError
+from openerp import models
+from openerp.addons.base_idu.tools import direcciones
+from openerp.tools.translate import _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class fields_security_mixin(models.AbstractModel):
+    _name = 'models.fields.security.mixin'
+
+    _write_fields_whitelist = False
+
+    def check_field_access_rights(self, cr, user, operation, fields, context=None):
+        """Just allow to write on specific fields for some groups referenced at _write_fields_whitelist"""
+
+        res = super(fields_security_mixin, self).check_field_access_rights(cr, user, operation, fields, context)
+        if not hasattr(self, '_write_fields_whitelist'):
+            return res
+
+        whitelisted_groups = self._write_fields_whitelist.keys()
+        if operation == 'write' and whitelisted_groups and self.user_has_groups(cr, user, groups=','.join(whitelisted_groups), context=context):
+            fields_set = set(fields)
+            for group in whitelisted_groups:
+                fields_allowed = set(self._write_fields_whitelist[group])
+                is_allowed = fields_allowed.issuperset(fields_set)
+                if self.user_has_groups(cr, user, groups=group, context=context) and not is_allowed:
+                    _logger.warning('Access Denied by ACLs for operation: %s, uid: %s, model: %s, fields: %s',
+                        operation, user, self._name, ', '.join( fields_set - fields_allowed)
+                    )
+                    raise AccessError(
+                        _('The requested operation cannot be completed due to security restrictions. '
+                        'Please contact your system administrator.\n\n(Document type: %s, Operation: %s)') % \
+                        (self._description, operation)
+                    )
+        return res
