242d7047b3dee54e237c98f582b28c98df570b69
Cinxgler Mariaca Minda <cinxgler@gmail.com>
2016-01-28 10:13:36 -0500

    utils/import_odoo_xml.py soporta actualización de <template>

--- utils/import_odoo_xml.py
+++ utils/import_odoo_xml.py
@@ -46,37 +46,78 @@ def importar_xml(options):
         elements = root.findall(".//record[@id='{0}']".format(options.record_id))
     else:
         elements = root.iter('record')
+    update_records(elements, odoo, options)
+
+    elements = None
+    if options.record_id:
+        elements = root.findall(".//template[@id='{0}']".format(options.record_id))
+    else:
+        elements = root.iter('template')
+    update_templates(elements, odoo, options)
+
+
+def update_templates(elements, odoo, options):
     for record in elements:
         record_model = record.get('model')
         record_id = record.get('id')
-        if record_model == 'ir.ui.view':
-            view_id = odoo.model('ir.model.data').browse([
-                'module = {0}'.format(options.module_name),
-                'name = {0}'.format(record_id),
-            ]).res_id
-            if not view_id:
-                _logger.error('No se encontró la vista a actualizar {1}.{2}'.format(options.module_name, record_id))
+        view_id = odoo.model('ir.model.data').browse([
+            'module = {0}'.format(options.module_name),
+            'name = {0}'.format(record_id),
+        ]).res_id
+        if not view_id:
+            _logger.error('No se encontró la vista a actualizar {1}.{2}'.format(options.module_name, record_id))
+        view_arch = ET.tostring(record)
+        view_arch = view_arch.strip()
+        view_arch = re.sub('^<template.*>', '', view_arch)
+        view_arch = re.sub('</template>$', '', view_arch)
+        view_arch = view_arch.strip()
+        if view_arch:
+            _logger.info('Updating view {0} {1}.{2}'.format(view_id, options.module_name, record_id))
+            _logger.debug("'{0}'".format(view_arch))
+            try:
+                odoo.model('ir.ui.view').write(view_id, {
+                    'arch': view_arch
+                })
+            except Exception as e:
+                _logger.error('Updating view {0} {1}.{2}'.format(view_id, options.module_name, record_id))
+                _logger.error(e.faultCode)
+                _logger.debug(e.faultString)
 
-            view_arch = None
-            for field in record.findall('field'):
-                if field.get('name') == 'arch':
-                    view_arch = ET.tostring(field)
-                    view_arch = view_arch.strip()
-                    view_arch = re.sub('^<field name="arch" type="xml">', '', view_arch)
-                    view_arch = re.sub('</field>$', '', view_arch)
-                    view_arch = view_arch.strip()
-                    break
-            if view_arch:
-                _logger.info('Updating view {0} {1}.{2}'.format(view_id, options.module_name, record_id))
-                _logger.debug("'{0}'".format(view_arch))
-                try:
-                    odoo.model('ir.ui.view').write(view_id, {
-                        'arch': view_arch
-                    })
-                except Exception as e:
-                    _logger.error('Updating view {0} {1}.{2}'.format(view_id, options.module_name, record_id))
-                    _logger.error(e.faultCode)
-                    _logger.debug(e.faultString)
+def update_records(elements, odoo, options):
+    for record in elements:
+        record_model = record.get('model')
+        if record_model == 'ir.ui.view':
+            update_ir_model_data(record, odoo, options)
+
+def update_ir_model_data(record, odoo, options):
+    record_id = record.get('id')
+    view_id = odoo.model('ir.model.data').browse([
+        'module = {0}'.format(options.module_name),
+        'name = {0}'.format(record_id),
+    ]).res_id
+    if not view_id:
+        _logger.error('No se encontró la vista a actualizar {1}.{2}'.format(options.module_name, record_id))
+
+    view_arch = None
+    for field in record.findall('field'):
+        if field.get('name') == 'arch':
+            view_arch = ET.tostring(field)
+            view_arch = view_arch.strip()
+            view_arch = re.sub('^<field name="arch" type="xml">', '', view_arch)
+            view_arch = re.sub('</field>$', '', view_arch)
+            view_arch = view_arch.strip()
+            break
+    if view_arch:
+        _logger.info('Updating view {0} {1}.{2}'.format(view_id, options.module_name, record_id))
+        _logger.debug("'{0}'".format(view_arch))
+        try:
+            odoo.model('ir.ui.view').write(view_id, {
+                'arch': view_arch
+            })
+        except Exception as e:
+            _logger.error('Updating view {0} {1}.{2}'.format(view_id, options.module_name, record_id))
+            _logger.error(e.faultCode)
+            _logger.debug(e.faultString)
 
 def conectar_odoo_openerp(server, db_name, user, password):
     _logger.debug('Contectando a: {0} {1}'.format(server, db_name));
