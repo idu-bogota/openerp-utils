f25d686401b669be5ff3c48bfc736955a1bf26ef
Cinxgler Mariaca Minda <cinxgler.mariaca@idu.gov.co>
2016-05-05 14:59:41 -0500

    Adicionado blacklist en models.fields.security.mixin

--- src/model_security/model_security.py
+++ src/model_security/model_security.py
@@ -32,30 +32,45 @@ _logger = logging.getLogger(__name__)
 class fields_security_mixin(models.AbstractModel):
     _name = 'models.fields.security.mixin'
 
-    _write_fields_whitelist = False
+    _write_fields_whitelist = {}
+    _write_fields_blacklist = {}
 
     def check_field_access_rights(self, cr, user, operation, fields, context=None):
         """Just allow to write on specific fields for some groups referenced at _write_fields_whitelist"""
 
         res = super(fields_security_mixin, self).check_field_access_rights(cr, user, operation, fields, context)
-        if not hasattr(self, '_write_fields_whitelist'):
-            return res
-
-        whitelisted_groups = self._write_fields_whitelist.keys()
-        if operation == 'write' and whitelisted_groups and self.user_has_groups(cr, user, groups=','.join(whitelisted_groups), context=context):
-            fields_set = set(fields)
-            for group in whitelisted_groups:
-                fields_allowed = set(self._write_fields_whitelist[group])
-                is_allowed = fields_allowed.issuperset(fields_set)
-                if self.user_has_groups(cr, user, groups=group, context=context) and not is_allowed:
-                    _logger.warning('Access Denied by ACLs for operation: %s, uid: %s, model: %s, fields: %s',
-                        operation, user, self._name, ', '.join( fields_set - fields_allowed)
-                    )
-                    raise AccessError(
-                        _('The requested operation cannot be completed due to security restrictions. '
-                        'Please contact your system administrator.\n\n(Document type: %s, Operation: %s)') % \
-                        (self._description, operation)
-                    )
+        if hasattr(self, '_write_fields_whitelist') and self._write_fields_whitelist:
+            whitelisted_groups = self._write_fields_whitelist.keys()
+            if operation == 'write' and whitelisted_groups and self.user_has_groups(cr, user, groups=','.join(whitelisted_groups), context=context):
+                fields_set = set(fields)
+                for group in whitelisted_groups:
+                    fields_allowed = set(self._write_fields_whitelist[group])
+                    is_allowed = fields_allowed.issuperset(fields_set)
+                    if self.user_has_groups(cr, user, groups=group, context=context) and not is_allowed:
+                        _logger.warning('Access Denied by ACLs for operation: %s, uid: %s, model: %s, fields: %s',
+                            operation, user, self._name, ', '.join( fields_set - fields_allowed)
+                        )
+                        raise AccessError(
+                            _('The requested operation cannot be completed due to security restrictions. '
+                            'Please contact your system administrator.\n\n(Document type: %s, Operation: %s)') % \
+                            (self._description, operation)
+                        )
+        if hasattr(self, '_write_fields_blacklist') and self._write_fields_blacklist:
+            blacklisted_groups = self._write_fields_blacklist.keys()
+            if operation == 'write' and blacklisted_groups and self.user_has_groups(cr, user, groups=','.join(blacklisted_groups), context=context):
+                fields_set = set(fields)
+                for group in blacklisted_groups:
+                    fields_denied = set(self._write_fields_blacklist[group])
+                    is_denied = fields_denied.issuperset(fields_set)
+                    if self.user_has_groups(cr, user, groups=group, context=context) and is_denied:
+                        _logger.warning('Access Denied by ACLs for operation: %s, uid: %s, model: %s, fields: %s',
+                            operation, user, self._name, ', '.join( fields_set - fields_denied)
+                        )
+                        raise AccessError(
+                            _('The requested operation cannot be completed due to security restrictions. '
+                            'Please contact your system administrator.\n\n(Document type: %s, Operation: %s)') % \
+                            (self._description, operation)
+                        )
         return res
 
 
